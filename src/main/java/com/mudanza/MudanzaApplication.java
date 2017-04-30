93d1e1e18d42d3e50aab571ac4b47425c9c0c588
afsantaliestra <afsantaliestra@gmail.com>
2017-04-30 14:28:47 +0200

    prueba subida con log

--- src/main/java/com/mudanza/MudanzaApplication.java
+++ src/main/java/com/mudanza/MudanzaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MudanzaApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MudanzaApplication.class, args);
 	}
 }
